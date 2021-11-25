1468eb8aeba47c3977ed6c25aa5a8a84eb52be33
Lars Muldjord <muldjordlars@gmail.com>
2021-11-25 12:40:57 +0100

    Now includes README.md

--- build_appimage.sh
+++ build_appimage.sh
@@ -11,6 +11,7 @@ cp LithoMaker AppImage/
 cp -a examples AppImage/
 cp AUTHORS AppImage/
 cp LICENSE AppImage/
+cp README.md AppImage/
 cp default.png AppImage/
 cp default.desktop AppImage/
 ../linuxdeployqt-continuous-x86_64.AppImage AppImage/LithoMaker -appimage
